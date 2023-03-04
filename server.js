d6f83676bb1082eedf2a2a639c2f2d4950488396
Jefferson Chen <me@chenjefferson.com>
2023-03-03 16:00:02 -0800

    add all dumb server files

---
+++ server.js
@@ -0,0 +1,14 @@
+const { hostname } = require('os');
+const http = require('http');
+const message = 'Hello World\n';
+const port = 8080;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(message);
+});
+
+// note the port, hostname order
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname()}:${port}/`);
+});
